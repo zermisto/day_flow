74c0bd0f0608f40f4d63d01bf77e8234ebadcb9e
zermisto <pdangkaj@cmkl.ac.th>
2023-10-22 17:07:14 +0700

    added create_event.py functionality

--- src/create_event.py
+++ src/create_event.py
@@ -2,100 +2,175 @@
 # Create Event Function GUI for the Personal Calendar Application
 # Created by King, 19th October 2023
 
+import  sqlite3
+import uuid
+from event_class import eventClass
+from sqlite_demo import insert_event
+
+connection = sqlite3.connect("eventDB.db")
+cursor = connection.cursor()
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class CreateEventPopup(QtWidgets.QWidget):
-    def setupUi(self, Form):
+    def set_up_ui(self, Form):
         Form.setObjectName("Form")
         Form.resize(440, 481)
 
-        # Main widget
+        # Main widget box
         self.widget = QtWidgets.QWidget(Form)
-        self.widget.setGeometry(QtCore.QRect(50, 40, 351, 381))
-        self.widget.setObjectName("widget")
+        self.widget.setGeometry(QtCore.QRect(50, 40, 351, 391))
 
-        # Event Title TextEdit
-        self.textEdit_7 = QtWidgets.QTextEdit(self.widget)
-        self.textEdit_7.setGeometry(QtCore.QRect(0, 0, 351, 31))
-        self.textEdit_7.setObjectName("textEdit_7")
-        self.textEdit_7.setPlaceholderText("Event Title")
+        # Event Title 
+        self.event_title_text = QtWidgets.QTextEdit(self.widget)
+        self.event_title_text.setGeometry(QtCore.QRect(0, 0, 351, 31))
+        self.event_title_text.setPlaceholderText("Event Title")
 
-        # DateEdit
-        self.dateEdit = QtWidgets.QDateEdit(self.widget)
-        self.dateEdit.setGeometry(QtCore.QRect(0, 40, 121, 31))
-        self.dateEdit.setDisplayFormat("yyyy/mm/dd")
-        self.dateEdit.setObjectName("dateEdit")
+        # Label Start Date 
+        self.label_start_date = QtWidgets.QLabel(self.widget)
+        self.label_start_date.setGeometry(QtCore.QRect(0, 40, 121, 31))    
 
-        # Description TextEdit
-        self.textEdit_5 = QtWidgets.QTextEdit(self.widget)
-        self.textEdit_5.setGeometry(QtCore.QRect(0, 80, 351, 91))
-        self.textEdit_5.setObjectName("textEdit_5")
-        self.textEdit_5.setPlaceholderText("Description")
+        # Start Date 
+        self.start_date_widget = QtWidgets.QDateEdit(self.widget)
+        self.start_date_widget.setGeometry(QtCore.QRect(65, 40, 91, 31))
+        self.start_date_widget.setDisplayFormat("yyyy-MM-dd")
+        self.start_date_widget.setDateTime(QtCore.QDateTime.currentDateTime())
 
-        # Location TextEdit
-        self.textEdit_6 = QtWidgets.QTextEdit(self.widget)
-        self.textEdit_6.setGeometry(QtCore.QRect(0, 180, 351, 51))
-        self.textEdit_6.setObjectName("textEdit_6")
-        self.textEdit_6.setPlaceholderText("Location")
+        # Label Start Time 
+        self.label_start_time = QtWidgets.QLabel(self.widget)
+        self.label_start_time.setGeometry(QtCore.QRect(0, 80, 121, 31))
+        
+        # Start Time 
+        self.start_time_widget = QtWidgets.QTimeEdit(self.widget)
+        self.start_time_widget.setGeometry(QtCore.QRect(65, 80, 81, 31))
+        self.start_time_widget.setDisplayFormat("hh:mm")
+        self.start_time_widget.setDateTime(QtCore.QDateTime.currentDateTime())
 
-        # Repeat Count Label
-        self.label_4 = QtWidgets.QLabel(self.widget)
-        self.label_4.setGeometry(QtCore.QRect(230, 250, 101, 20))
-        self.label_4.setObjectName("label_4")
+        # Label End Date 
+        self.label_end_state = QtWidgets.QLabel(self.widget)
+        self.label_end_state.setGeometry(QtCore.QRect(170, 40, 121, 31))
 
-        # Repeat Pattern ComboBox
-        self.comboBox = QtWidgets.QComboBox(self.widget)
-        self.comboBox.setGeometry(QtCore.QRect(10, 270, 73, 22))
-        self.comboBox.setObjectName("comboBox")
-        self.comboBox.addItems(["Daily", "Weekly", "Monthly", "Yearly"])
+        # End Date
+        self.end_date_widget = QtWidgets.QDateEdit(self.widget)
+        self.end_date_widget.setGeometry(QtCore.QRect(230, 40, 91, 31))
+        self.end_date_widget.setDisplayFormat("yyyy-MM-dd")
+        self.end_date_widget.setDateTime(QtCore.QDateTime.currentDateTime())
 
-        # Days of the Week ComboBox
-        self.comboBox_2 = QtWidgets.QComboBox(self.widget)
-        self.comboBox_2.setGeometry(QtCore.QRect(120, 270, 73, 22))
-        self.comboBox_2.setObjectName("comboBox_2")
-        self.comboBox_2.addItems(["Mon", "Tue", "Wed", "Thu", "Fri"])
+        # Label End Time
+        self.label_end_time = QtWidgets.QLabel(self.widget)
+        self.label_end_time.setGeometry(QtCore.QRect(170, 80, 121, 31))
 
-        # Repeat Every Label
-        self.label_3 = QtWidgets.QLabel(self.widget)
-        self.label_3.setGeometry(QtCore.QRect(120, 250, 101, 20))
-        self.label_3.setObjectName("label_3")
+        # End Time
+        self.end_time_widget = QtWidgets.QTimeEdit(self.widget)
+        self.end_time_widget.setGeometry(QtCore.QRect(230, 80, 81, 31))
+        self.end_time_widget.setDisplayFormat("hh:mm")
+        self.end_time_widget.setDateTime(QtCore.QDateTime.currentDateTime())
 
-        # Repeat Pattern Label
-        self.label_2 = QtWidgets.QLabel(self.widget)
-        self.label_2.setGeometry(QtCore.QRect(0, 250, 101, 20))
-        self.label_2.setObjectName("label_2")
+        # Description 
+        self.description_text = QtWidgets.QTextEdit(self.widget)
+        self.description_text.setGeometry(QtCore.QRect(0, 120, 351, 91))
+        self.description_text.setPlaceholderText("Description")
 
-        # Ends After Label
-        self.label = QtWidgets.QLabel(self.widget)
-        self.label.setGeometry(QtCore.QRect(230, 290, 61, 16))
-        self.label.setObjectName("label")
+        # Location 
+        self.location_text = QtWidgets.QTextEdit(self.widget)
+        self.location_text.setGeometry(QtCore.QRect(0, 220, 351, 51))
+        self.location_text.setPlaceholderText("Location")
+    
+        # Label Repeat Pattern 
+        self.label_repeat_pattern = QtWidgets.QLabel(self.widget)
+        self.label_repeat_pattern.setGeometry(QtCore.QRect(0, 290, 101, 20))
 
-        # Number of Occurrences SpinBox
-        self.spinBox = QtWidgets.QSpinBox(self.widget)
-        self.spinBox.setGeometry(QtCore.QRect(290, 290, 42, 22))
-        self.spinBox.setObjectName("spinBox")
+        # Repeat Pattern 
+        self.comboBox_widget = QtWidgets.QComboBox(self.widget)
+        self.comboBox_widget.setGeometry(QtCore.QRect(10, 310, 73, 22))
+        self.comboBox_widget.addItems(["Daily", "Weekly", "Monthly", "Yearly"])
+
+        # Label Repeat Every 
+        self.label_repeat_every = QtWidgets.QLabel(self.widget)
+        self.label_repeat_every.setGeometry(QtCore.QRect(120, 290, 101, 20))
+
+        # Repeat Every
+        self.comboBox_2_widget = QtWidgets.QComboBox(self.widget)
+        self.comboBox_2_widget.setGeometry(QtCore.QRect(120, 310, 73, 22))
+        self.comboBox_2_widget.addItems(["Mon", "Tue", "Wed", "Thu", "Fri"])
+
+        # Label Repeat Count 
+        self.label_repeat_count = QtWidgets.QLabel(self.widget)
+        self.label_repeat_count.setGeometry(QtCore.QRect(230, 290, 101, 20))
 
         # Forever CheckBox
-        self.checkBox = QtWidgets.QCheckBox(self.widget)
-        self.checkBox.setGeometry(QtCore.QRect(230, 270, 91, 20))
-        self.checkBox.setObjectName("checkBox")
-        self.checkBox.setText("Forever")
+        self.checkBox_widget = QtWidgets.QCheckBox(self.widget)
+        self.checkBox_widget.setGeometry(QtCore.QRect(230, 310, 91, 20))
+        self.checkBox_widget.setText("Forever")
+        # if user click forever, disable the spinBox
+        self.checkBox_widget.stateChanged.connect(lambda: self.spinBox_widget.setDisabled(self.checkBox_widget.isChecked())) 
 
-        self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
+        # Ends After Label
+        self.label_ends_after = QtWidgets.QLabel(self.widget)
+        self.label_ends_after.setGeometry(QtCore.QRect(230, 330, 61, 16))
 
-    def retranslateUi(self, Form):
+        # Number of Event Occurrences SpinBox
+        self.spinBox_widget = QtWidgets.QSpinBox(self.widget)
+        self.spinBox_widget.setGeometry(QtCore.QRect(290, 330, 42, 22))
+        self.spinBox_widget.setMinimum(1)
+
+        # OK Button
+        self.ok_button_widget = QtWidgets.QPushButton(self.widget)
+        self.ok_button_widget.setGeometry(QtCore.QRect(20, 360, 93, 28)) 
+
+        # Cancel Button
+        self.cancel_button_widget = QtWidgets.QPushButton(self.widget)
+        self.cancel_button_widget.setGeometry(QtCore.QRect(230, 360, 93, 28))
+
+        self.retranslate_ui(Form)
+        # if user click ok, call getInputFromUser, and close form
+        self.ok_button_widget.clicked.connect(lambda: insert_event(self.get_input_from_user()))
+        self.ok_button_widget.clicked.connect(Form.close)
+
+        # if user click cancel, close form
+        self.cancel_button_widget.clicked.connect(Form.close)
+
+        QtCore.QMetaObject.connectSlotsByName(Form) 
+
+    def retranslate_ui(self, Form):
         Form.setWindowTitle("Form")
-        self.label_4.setText("Repeat Count?")
-        self.label_2.setText("Repeat Pattern?")
-        self.label_3.setText("Repeat Every?")
-        self.label.setText("Ends After")
+        self.label_start_date.setText("Start Date")
+        self.label_start_time.setText("Start Time")
+        self.label_end_state.setText("End Date")
+        self.label_end_time.setText("End Time")
+        self.label_repeat_count.setText("Repeat Count?")
+        self.label_repeat_pattern.setText("Repeat Pattern?")
+        self.label_repeat_every.setText("Repeat Every?")
+        self.label_ends_after.setText("Ends After")
+        self.ok_button_widget.setText("OK")
+        self.cancel_button_widget.setText("CANCEL")
+
+    def get_input_from_user(self):
+        id = uuid.uuid4().int & (1<<16)-1 # 16-bit random id
+        event_name = self.event_title_text.toPlainText()  # get event name
+        start_date = self.start_date_widget.text()
+        end_date = self.end_date_widget.text()
+        start_time = self.start_time_widget.text()
+        end_time = self.end_time_widget.text()
+        description = self.description_text.toPlainText()
+        location = self.location_text.toPlainText()
+        repeat_pattern = self.comboBox_widget.currentText()
+        repeat_every = self.comboBox_2_widget.currentText()
+        forever = self.checkBox_widget.isChecked()
+        # if user click forever, repeat_count = -1 and disable the spinBoxq
+        if forever:
+            repeat_count = -1
+        else:
+            repeat_count = self.spinBox_widget.value()
+
+        event_data = eventClass(id, event_name, start_date, end_date, start_time, end_time, description, location, repeat_every, repeat_pattern, repeat_count)
+        return event_data
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     Form = QtWidgets.QWidget()
     ui = CreateEventPopup()
-    ui.setupUi(Form)
+    ui.set_up_ui(Form)
     Form.show()
     sys.exit(app.exec_())
--- src/sqlite_demo.py
+++ src/sqlite_demo.py
@@ -4,7 +4,7 @@
 # Created by King, 1st October 2023
 
 import sqlite3
-from event_manager import event
+from event_class import eventClass
 
 # creating database file called eventDB.db 
 # (change to eventDB.db to :memory: to create a database in RAM for testing)
@@ -22,11 +22,15 @@ with connection:    #creating table called events
             end_time text,
             description text,
             location text,
-            repeat_every text,
+            repeat_every text, 
             repeat_pattern text,
             repeat_count integer
             )""")
     
+# repeat_every can be "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"
+# repeat_pattern can be "D", "W", "M", "Y" (daily, weekly, monthly, yearly)
+# repeat_count is the number of times the event repeats (0 for infinite)
+
 # insert event into table
 def insert_event(event):
     with connection:
@@ -42,7 +46,8 @@ def edit_event(event):
     with connection:
         cursor.execute("""UPDATE events SET name=:name, start_date=:start_date, end_date=:end_date, start_time=:start_time,
                         end_time=:end_time, description=:description, location=:location, repeat_every=:repeat_every,
-                        repeat_pattern=:repeat_pattern, repeat_count=:repeat_count WHERE id=:id""", {'id': event.id,
+                        repeat_pattern=:repeat_pattern, repeat_count=:repeat_count WHERE id=:id""", 
+                        {'id': event.id,
                         'name': event.name, 'start_date': event.start_date, 'end_date': event.end_date,
                         'start_time': event.start_time, 'end_time': event.end_time, 'description': event.description,
                         'location': event.location, 'repeat_every': event.repeat_every, 'repeat_pattern': event.repeat_pattern,
@@ -67,31 +72,31 @@ def get_event_by_start_date(start_date):
 ## TEST CODE ##
 
 # update event in table
-event1 = event(1, 'Sports Day', '2023-09-27', '2023-09-27', '09:00', '17:00', 'Sports Day', 'Sports Hall', 'Once', 'Once', 0)
-event2 = event(2, 'Meeting', '2023-10-05', '2023-10-05', '09:00', '17:00', 'Meeting', 'Meeting Room', 'Wed', 'W', 10)
-event3 = event(3, 'Open House', '2023-09-09', '2023-10-09', '09:00', '17:00', 'Open House', 'Hall', 'Once', 'Once', 0)
+# event1 = eventClass(1, 'Sports Day', '2023-09-27', '2023-09-27', '09:00', '17:00', 'Sports Day', 'Sports Hall', 'Once', 'Once', 0)
+# event2 = eventClass(2, 'Meeting', '2023-10-05', '2023-10-05', '09:00', '17:00', 'Meeting', 'Meeting Room', 'Wed', 'W', 10)
+# event3 = eventClass(3, 'Open House', '2023-09-09', '2023-10-09', '09:00', '17:00', 'Open House', 'Hall', 'Once', 'Once', 0)
 
-insert_event(event1)
-insert_event(event2)
-insert_event(event3)
+# insert_event(event1)
+# insert_event(event2)
+# insert_event(event3)
 
-# search for events with name containing "Sports Day"
-events = get_event_by_name("Sports Day")
-print(events)
+# # search for events with name containing "Sports Day"
+# events = get_event_by_name("Sports Day")
+# print(events)
 
-# search for events with date containing "2023-10-05"
-events = get_event_by_start_date("2023-10-05")
-print(events)
+# # search for events with date containing "2023-10-05"
+# events = get_event_by_start_date("2023-10-05")
+# print(events)
 
-# update event with id 1
-event1.name = "Sports Day 2023"
-edit_event(event1)
+# # update event with id 1
+# event1.name = "Sports Day 2023"
+# edit_event(event1)
 
-print("\n*********************************\n")
-# search for all events
-cursor.execute("SELECT * FROM events")
-for row in cursor.fetchall():
-    print(row)
+# print("\n*********************************\n")
+# # # search for all events
+# cursor.execute("SELECT * FROM events")
+# for row in cursor.fetchall():
+#     print(row)
 
 
 
